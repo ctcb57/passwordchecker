77974c84495693403722fc6b8d0718c69ccf45fc
charles clark <charles@pop-os.localdomain>
2020-01-08 17:58:13 -0600

    Create initial base functions and connected to the API.  Response confirmed

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,12 @@
 import requests
 
-url = 'https://api.pwnedpasswords.com/range/' + 'password123'
+url = 'https://api.pwnedpasswords.com/range/' + '0E517'
 res = requests.get(url)
 print(res)
+
+def request_api_data(query_char):
+    pass
+
+def pwned_api_check(password):
+    #check password if it exists in the API response
+    pass
