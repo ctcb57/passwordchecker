fd91ac9242d65d687419fbbfa4179da779a6b631
charles clark <charles@pop-os.localdomain>
2020-01-08 19:20:01 -0600

    Modified script to check if the password is in the response from the API.  Functionality confirmed

--- checkmypass.py
+++ checkmypass.py
@@ -1,4 +1,5 @@
 import requests
+import hashlib
 
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
@@ -7,7 +8,18 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
     return res
 
-    
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            pass
+
 def pwned_api_check(password):
-    #check password if it exists in the API response
-    pass
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    print(response)
+    return get_password_leaks_count(response, tail)
+
+pwned_api_check('12345')
